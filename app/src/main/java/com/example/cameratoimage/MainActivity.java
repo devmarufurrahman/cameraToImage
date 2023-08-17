2140c67ea2185ad96720c3ce629e96eaf2d96418
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-08-17 18:34:15 +0600

    camera data show done

--- app/src/main/java/com/example/cameratoimage/MainActivity.java
+++ app/src/main/java/com/example/cameratoimage/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.cameratoimage;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int CAMERA_REQ_CODE = 100;
+    ImageView imgView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         imgView = findViewById(R.id.imgView);
+        Button btnCamera = findViewById(R.id.btnCamera);
+
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent icamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(icamera,CAMERA_REQ_CODE);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+                if (requestCode == CAMERA_REQ_CODE){
+
+                    Bitmap img = (Bitmap) data.getExtras().get("data");
+                    imgView.setImageBitmap(img);
+                }
+        }
     }
 }
